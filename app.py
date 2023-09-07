f9213c61d01770baf105a8ed15cdb39ab0aa3273
Cole Hagen <61998370+chags1313@users.noreply.github.com>
2023-09-06 23:30:30 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,57 +1,61 @@
-from flask import Flask, request, jsonify, send_file
+from flask import Flask, request, jsonify
 import mediapipe as mp
 import numpy as np
 import cv2
-from io import BytesIO
-import os
 
 app = Flask(__name__)
 
 mp_pose = mp.solutions.pose
 pose = mp_pose.Pose()
 
+# Define custom drawing specifications for white keypoints and lines
+white_landmark_drawing_spec = mp.solutions.drawing_utils.DrawingSpec(color=(255, 255, 255), thickness=2, circle_radius=2)
+white_connection_drawing_spec = mp.solutions.drawing_utils.DrawingSpec(color=(255, 255, 255), thickness=2)
+
 @app.route('/process_image', methods=['POST'])
 def process_image():
-    # Get the image from the request
-    image_stream = request.files['image'].read()
-    image_np = np.frombuffer(image_stream, np.uint8)
-    image = cv2.imdecode(image_np, cv2.IMREAD_COLOR)
+    try:
+        # Get the image from the request
+        image_stream = request.files['image'].read()
+        image_np = np.frombuffer(image_stream, np.uint8)
+        image = cv2.imdecode(image_np, cv2.IMREAD_COLOR)
 
-    # Define custom drawing specifications
-    white_landmark_drawing_spec = mp.solutions.drawing_utils.DrawingSpec(color=(255, 255, 255), thickness=2, circle_radius=2)
-    white_connection_drawing_spec = mp.solutions.drawing_utils.DrawingSpec(color=(255, 255, 255), thickness=2)
-    
-    # Process the image using MediaPipe
-    results = pose.process(image)
-    
-    # Draw the pose landmarks on the image
-    if results.pose_landmarks:
-        mp.solutions.drawing_utils.draw_landmarks(
-            image, 
-            results.pose_landmarks, 
-            mp_pose.POSE_CONNECTIONS,
-            landmark_drawing_spec=white_landmark_drawing_spec,
-            connection_drawing_spec=white_connection_drawing_spec
-        )
-    
+        # Process the image using MediaPipe
+        results = pose.process(image)
+
+        # Draw the pose landmarks on the image
+        if results.pose_landmarks:
+            mp.solutions.drawing_utils.draw_landmarks(
+                image, 
+                results.pose_landmarks, 
+                mp_pose.POSE_CONNECTIONS,
+                landmark_drawing_spec=white_landmark_drawing_spec,
+                connection_drawing_spec=white_connection_drawing_spec
+            )
+
+        # Convert the image back to a format suitable for sending via HTTP
+        _, img_encoded = cv2.imencode('.jpg', image)
+        img_bytes = img_encoded.tobytes()
+
         # Extract keypoints data
         keypoints = []
-        for landmark in results.pose_landmarks.landmark:
-            keypoints.append({
-                'x': landmark.x,
-                'y': landmark.y,
-                'z': landmark.z,
-                'visibility': landmark.visibility
-            })
-    else:
-        keypoints = []
+        if results.pose_landmarks:
+            for landmark in results.pose_landmarks.landmark:
+                keypoints.append({
+                    'x': landmark.x,
+                    'y': landmark.y,
+                    'z': landmark.z,
+                    'visibility': landmark.visibility
+                })
 
-    # Send the processed image and keypoints data as response
-    return jsonify({
-        'image': img_bytes.decode('latin1'),  # Convert bytes to string for JSON serialization
-        'keypoints': keypoints
-    })
+        return jsonify({
+            'image': img_bytes.decode('latin1'),  # Convert bytes to string for JSON serialization
+            'keypoints': keypoints
+        })
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
+    import os
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
